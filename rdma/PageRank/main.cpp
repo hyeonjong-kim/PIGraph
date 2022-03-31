6c09bd8c2004bc015c9e82c4791e4bf955292756
hyeonjong-kim <cowkey206@gmail.com>
2022-03-31 15:03:29 +0900

    22/03/31_4

--- rdma/PageRank/main.cpp
+++ rdma/PageRank/main.cpp
@@ -254,10 +254,15 @@ int main(int argc, const char *argv[]){
   	}
 	futures.clear();
 	
+	for (size_t i = 0; i < num_host; i++)
+	{
+		t[i].SendCheckmsg();
+	}
+	
 	
 	for(int j = 0; j < num_host; j++){
 		auto f = [&t, j](){
-			t[j].SendCheckmsg();
+			
 			string s = "";
 			while(s.compare("1\n")!= 0){
 				s = t[j].ReadCheckMsg();
