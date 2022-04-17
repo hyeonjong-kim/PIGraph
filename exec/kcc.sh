385e2d8795109dc3939f2769213350e778c4a04d
hyeonjong-kim <cowkey206@gmail.com>
2022-04-18 03:42:01 +0900

    22/04/18_6

--- exec/kcc.sh
+++ exec/kcc.sh
@@ -1,18 +1,18 @@
-./test_kcc.sh 256 ../../data/web-Google.txt 30 10 6 1 web-Google
+./test_kcc.sh 256 ../../data/Amazon0601.txt 30 10 6 1 Amazon0601
 sleep 120s
 ./test_kcc.sh 256 ../../data/wiki-Talk.txt 30 10 6 1 wiki-Talk
 sleep 120s
 ./test_kcc.sh 2048 ../../data/soc-LiveJournal1.txt 30 10 6 1 soc-LiveJournal
 sleep 120s
 
-./test_kcc.sh 256 ../../data/web-Google.txt 30 10 4 1 web-Google
+./test_kcc.sh 256 ../../data/Amazon0601.txt 30 10 4 1 Amazon0601
 sleep 120s
 ./test_kcc.sh 256 ../../data/wiki-Talk.txt 30 10 4 1 wiki-Talk
 sleep 120s
 ./test_kcc.sh 2048 ../../data/soc-LiveJournal1.txt 30 10 4 1 soc-LiveJournal
 sleep 120s
 
-./test_kcc.sh 256 ../../data/web-Google.txt 30 10 2 1 web-Google
+./test_kcc.sh 256 ../../data/Amazon0601.txt 30 10 2 1 Amazon0601
 sleep 120s
 ./test_kcc.sh 256 ../../data/wiki-Talk.txt 30 10 2 1 wiki-Talk
 sleep 120s
