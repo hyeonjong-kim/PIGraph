d6a7c3d2b715ddeb08e44314729ad80578fa684a
hyeonjong-kim <cowkey206@gmail.com>
2022-09-28 23:01:34 +0900

    22/09/28_8

--- bin/worker_main.cpp
+++ bin/worker_main.cpp
@@ -29,17 +29,52 @@ int main(int argc, const char *argv[]){
     Network* network = new Network();
     Processing* processing = new Processing();
     
+    struct timeval start_total = {};
+    struct timeval end_total = {};
+    struct timeval start_read_file = {};
+    struct timeval end_read_file = {};
+    struct timeval start_create_graph = {};
+    struct timeval end_create_graph = {};
+    struct timeval start_network_set = {};
+    struct timeval end_network_set = {};
+    struct timeval start_query = {};
+    struct timeval end_query = {};
 
+    gettimeofday(&start_total, NULL);
     setting->argParse(argc, argv);
     IO* io = new IO();
+    
+    gettimeofday(&start_read_file, NULL);
     io->setHDFSInfo((char*)(setting->HDFS_host.c_str()), setting->HDFS_port);
     string readFile = io->readHDFSFile((char*)(setting->filePath.c_str()), O_RDONLY);
+    gettimeofday(&end_read_file, NULL);
+
+    gettimeofday(&start_create_graph, NULL);
     graph->setInfo(setting->partitionOpt, setting->workers.size(), setting->thisHostNumber);
     graph->createGraph(readFile);
-    network->setNetwork(setting->networkType, setting->workers.size(), setting->workers, setting->port, graph->getRecvPos(), setting->mu, setting->numMutex, setting->thisHostNumber, graph->getMsgBuffer());
+    gettimeofday(&end_create_graph, NULL);
     
+    gettimeofday(&start_network_set, NULL);
+    network->setNetwork(setting->networkType, setting->workers.size(), setting->workers, setting->port, graph->getRecvPos(), setting->mu, setting->numMutex, setting->thisHostNumber, graph->getMsgBuffer());
+    gettimeofday(&end_network_set, NULL);
+
+    gettimeofday(&start_query, NULL);
     processing->setInfo(graph, network, setting->superstep, setting->numThread);
     processing->execute();
+    gettimeofday(&end_query, NULL);
+    gettimeofday(&end_total, NULL);
+
+    double time_total = end_total.tv_sec + end_total.tv_usec / 1000000.0 - start_total.tv_sec - start_total.tv_usec / 1000000.0;
+    double time_read_file = end_read_file.tv_sec + end_read_file.tv_usec / 1000000.0 - start_read_file.tv_sec - start_read_file.tv_usec / 1000000.0;
+    double time_create_graph = end_create_graph.tv_sec + end_create_graph.tv_usec / 1000000.0 - start_create_graph.tv_sec - start_create_graph.tv_usec / 1000000.0;
+    double time_network_setting = end_network_set.tv_sec + end_network_set.tv_usec / 1000000.0 - start_network_set.tv_sec - start_network_set.tv_usec / 1000000.0;
+    double time_query = end_query.tv_sec + end_query.tv_usec / 1000000.0 - start_query.tv_sec - start_query.tv_usec / 1000000.0;
+    
+    cerr << "Total execution time: " << time_total << endl;
+    cerr << "Time of Reading file: " << time_read_file << endl;
+    cerr << "Time of creating graph: " << time_create_graph << endl;
+    cerr << "Time of setting network: " << time_network_setting << endl;
+    cerr << "Time of processing graph: " << time_query << endl;
     
     /*
     map<int,double> finalResult;
